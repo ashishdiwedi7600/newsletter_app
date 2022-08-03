c79ecac3432b45d7e55aa52a9620cff1b0f86e8c
ashishdiwedi7600 <84765253+ashishdiwedi7600@users.noreply.github.com>
2022-08-03 19:20:43 +0530

    email correction

--- Controllers/postTemplateController.js
+++ Controllers/postTemplateController.js
@@ -29,7 +29,7 @@ exports.addTemplate = async (req, res) => {
 exports.sendNewsletter = async (req, res) => {
     const emailArr = req.body.emails
     const value = req.body.template_code
-    const sentTemplate = sendNewsletter(emailArr, JSON.parse(value))
+    const sentTemplate = await sendNewsletter(emailArr, JSON.parse(value))
     // const data = fs.readFileSync(emailJson, 'utf8');
     // let dataJson = JSON.parse(data);
     // emailArr.map(async (email) => {
--- Utils/Nodemailer2.js
+++ Utils/Nodemailer2.js
@@ -1,11 +1,14 @@
 var nodemailer = require('nodemailer');
 const { google } = require('googleapis');
+const xoauth2 = require('xoauth2')
+var smtpTransport = require('nodemailer-smtp-transport');
+
 
 // These id's and secrets should come from .env file.
-const CLIENT_ID = '525209363917-fi16gne2m0nv9ckgmpcinpg4fa13bklb.apps.googleusercontent.com';
-const CLEINT_SECRET = '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';
+const CLIENT_ID = '918008179236-0qs4oec00kggg4lv9l4draao1qk6krvn.apps.googleusercontent.com';
+const CLEINT_SECRET = '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';
 const REDIRECT_URI = 'https://developers.google.com/oauthplayground';
-const REFRESH_TOKEN = '1//04sl2PjYOpojyCgYIARAAGAQSNwF-L9IryBjiL1sUDd3hiJ47RIWMbnbFE1QNgjHlKZBRBhG0SL2E7IfSURbq7mXRKkHsUbrLVmE';
+const REFRESH_TOKEN = '1//04oKBDpQZ0Bg9CgYIARAAGAQSNwF-L9IrDQbaSAWA6zeY4dhdhyk0aEAhat0IzIVJjwrceHBq4B8Rm-2tbDaxONZc80p0d6FFy98';
 
 const oAuth2Client = new google.auth.OAuth2(
   CLIENT_ID,
@@ -15,15 +18,15 @@ const oAuth2Client = new google.auth.OAuth2(
 oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
 
 async function sendMail(emailsArr,code) {
-    console.log("success",emailsArr);
+    console.log("success",emailsArr,code);
     try {
       const accessToken = await oAuth2Client.getAccessToken();
-  
+
       const transport = nodemailer.createTransport({
         service: 'gmail',
         auth: {
           type: 'OAuth2',
-          user: 'Newsletter@kloudrac.com',
+          user: 'newsletter@kloudrac.com',
           clientId: CLIENT_ID,
           clientSecret: CLEINT_SECRET,
           refreshToken: REFRESH_TOKEN,
@@ -32,14 +35,16 @@ async function sendMail(emailsArr,code) {
       });
   
       const mailOptions = {
-        from: 'Newsletter@kloudrac.com',
+        from: 'newsletter@kloudrac.com',
         to: emailsArr,
         subject: 'Newsletter',
-        text: 'Newsletter from kloudrac',
+        text: 'Newsletter from Kloudrac',
         html: code
       };
   
+      console.log("result","result")
       const result = await transport.sendMail(mailOptions);
+      console.log("result",result)
       return result;
     } catch (error) {
       return error;
